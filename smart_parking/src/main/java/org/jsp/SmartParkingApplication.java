92e00bf2c243bf4fcdbc1d84a43ac72cb384b995
chenna <chennareddyganji99@gmail.com>
2026-09-05 22:13:11 +0530

    cors error fixing

--- smart_parking/src/main/java/org/jsp/SmartParkingApplication.java
+++ smart_parking/src/main/java/org/jsp/SmartParkingApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartParkingApplication {
 
 	public static void main(String[] args) {
+		 System.out.println(">>> DB_URL=[" + System.getenv("DB_URL") + "]");
+		 System.out.println(">>> DB_USERNAME=[" + System.getenv("DB_USERNAME") + "]");
 		SpringApplication.run(SmartParkingApplication.class, args);
 	}
 
