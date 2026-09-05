3efd342a5838c4a27d0e4ecbc8a99feb61ddc09e
chenna <chennareddyganji99@gmail.com>
2026-09-05 21:30:40 +0530

    Fix global CORS configuration

--- smart_parking/src/main/java/org/jsp/config/CorsConfig.java
+++ smart_parking/src/main/java/org/jsp/config/CorsConfig.java
@@ -17,15 +17,13 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                            "http://localhost:5173"
-                        )
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods(
-                            "GET",
-                            "POST",
-                            "PUT",
-                            "DELETE",
-                            "OPTIONS"
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
                         )
                         .allowedHeaders("*");
             }
