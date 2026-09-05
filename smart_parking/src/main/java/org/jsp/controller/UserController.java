e80296f708219a6189b454cf5070fc2d3e445a6c
chenna <chennareddyganji99@gmail.com>
2026-09-05 20:33:11 +0530

    Fix CORS configuration

--- smart_parking/src/main/java/org/jsp/controller/UserController.java
+++ smart_parking/src/main/java/org/jsp/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class UserController {
 	
