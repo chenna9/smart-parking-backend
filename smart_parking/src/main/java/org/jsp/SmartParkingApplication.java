948cbbc0b5e30d8dadb74effb0beb91ec21078a9
chenna <chennareddyganji99@gmail.com>
2026-09-06 10:59:43 +0530

    direct railway connection with mysql

--- smart_parking/src/main/java/org/jsp/SmartParkingApplication.java
+++ smart_parking/src/main/java/org/jsp/SmartParkingApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartParkingApplication {
 
 	public static void main(String[] args) {
-		 System.out.println(">>> DB_URL=[" + System.getenv("DB_URL") + "]");
-		 System.out.println(">>> DB_USERNAME=[" + System.getenv("DB_USERNAME") + "]");
+		
 		SpringApplication.run(SmartParkingApplication.class, args);
 	}
 
