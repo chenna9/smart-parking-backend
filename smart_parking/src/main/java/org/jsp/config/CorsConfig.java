50ec75d622e295336fbeafa5fd172fcf12e30918
chenna <chennareddyganji99@gmail.com>
2026-09-06 12:13:13 +0530

    Adding Vercel deployment URL to backend

--- smart_parking/src/main/java/org/jsp/config/CorsConfig.java
+++ smart_parking/src/main/java/org/jsp/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                        		"https://smart-parking-frontend-vert.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
